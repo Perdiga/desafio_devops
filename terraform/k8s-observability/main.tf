cc180b5ed94a727eaef8ca51bd360e923d005c4a
Mateus Perdigão <mateusperdigao@hotmail.com>
2024-03-05 02:50:03 +0000

    chore: try to use kube/config as path

--- terraform/k8s-observability/main.tf
+++ terraform/k8s-observability/main.tf
@@ -31,25 +31,13 @@ provider "aws" {
 }
 
 provider "helm" {
-  kubernetes {
-    host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
-    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.eks.outputs.cluster_name]
-      command     = "aws"
-    }
+   kubernetes {
+    config_path = "~/.kube/config"
   }
 }
 
-provider "kubernetes" {
-  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
-  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.eks.outputs.cluster_name]
-    command     = "aws"
-  }
+provider  kubernetes {
+    config_path = "~/.kube/config"
 }
 
 locals {
